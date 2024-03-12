b59a742197bf75c4057cd69e31279e084dd00f27
BendrGames <vanhaelst.ben@hotmail.com>
2024-03-12 10:13:26 +0100

    working on Spline code

--- Assets/Scripts/Splines/PlaceholderConnectorHitBox.cs
+++ Assets/Scripts/Splines/PlaceholderConnectorHitBox.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SplineTestGameLoop : MonoBehaviour
+public class PlaceholderConnectorHitBox : MonoBehaviour
 {
-   
+   public bool isOutput;
 }
--- Assets/Scripts/Splines/SplineView.cs
+++ Assets/Scripts/Splines/SplineView.cs
@@ -1,18 +1,18 @@
+using Dreamteck.Splines;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SplineView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SplineComputer _mySplineComputer;
+    private SplineMesh _mySplineMesh;
+
+    private void Awake()
     {
-        
+        _mySplineComputer = GetComponent<SplineComputer>();
+        _mySplineMesh = GetComponent<SplineMesh>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
