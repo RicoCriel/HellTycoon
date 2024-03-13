5d7f48c81988d86514547f7a9a2ad75db6503603
BendrGames <vanhaelst.ben@hotmail.com>
2024-03-13 10:33:40 +0100

    Lander spline test

--- Assets/Scripts/Splines/PlaceholderConnectorHitBox.cs
+++ Assets/Scripts/Splines/PlaceholderConnectorHitBox.cs
@@ -1,10 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 namespace Splines
 {
-    public class PlaceholderConnectorHitBox : MonoBehaviour
+    public class PlaceholderConnectorHitBox : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
     {
-        public bool isOutput;
+        [FormerlySerializedAs("isOutput")]
+        public bool IsSplineStart;
 
         [SerializeField]
         private Transform ConnectorPoint;
@@ -20,7 +23,7 @@ namespace Splines
                 _myMeshRenderer = GetComponent<MeshRenderer>();
 
 
-            if (isOutput)
+            if (IsSplineStart)
             {
                 _myMeshRenderer.material.color = Color.green;
             }
@@ -35,7 +38,7 @@ namespace Splines
             Vector3 position = ConnectorPoint.position;
             return new Vector3(position.x, 0, position.z);
         }
-        
+
         public Vector3 GetConnectorAnglePointSpline()
         {
             Vector3 position = ConnectorAnglePoint.position;
@@ -45,6 +48,39 @@ namespace Splines
         public Vector3 GetConnectorPointDirection()
         {
             return ConnectorPoint.forward;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+
+                if (!IsSplineStart)
+                {
+                    Debug.Log("StopDrawingSpline");
+                }
+                // Add your mouse down logic here
+            }
+        }
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                if (IsSplineStart)
+                {
+                    Debug.Log("StartDrawingSpline");
+                }
+                // Add your mouse down logic here
+            }
         }
     }
 }
