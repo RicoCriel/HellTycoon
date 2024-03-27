3cc6ae415dae422d0b9b64b1929839896fd21ae2
FlorisLeirs <floris.leirs@gmail.com>
2024-03-27 11:52:41 +0100

    Zooming with position and angle

--- Assets/Scripts/Camera/CameraSystem.cs
+++ Assets/Scripts/Camera/CameraSystem.cs
@@ -22,6 +22,25 @@ public class CameraSystem : MonoBehaviour
 
     [Header("Zoom")]
     [SerializeField] private float _zoomSpeed = 20f;
+    [SerializeField] private float _zoomSmoothing = 5f;
+    [SerializeField] private bool _zoomFOV = false;
+    [SerializeField] private bool _zoomFollowOffset = false;
+    [SerializeField] private bool _zoomLowerY = true;
+
+    [Header("FOV")]
+    [SerializeField] private float _targetFieldOfView = 50f;
+    [SerializeField] private float _minFieldOfView = 10f;
+    [SerializeField] private float _maxFieldOfView = 100f;
+
+    [Header("Follow Offset")]
+    [SerializeField] private Vector3 _followOffset;
+    [SerializeField] private float _minFollowOffset = 2;
+    [SerializeField] private float _maxFollowOffset = 20;
+
+    [Header("Lower Y")] 
+    [SerializeField] private float _minY = 5;
+    [SerializeField] private float _maxY = 40f;
+    
 
     private bool _dragPanMove = false;
     private Vector2 _lastMousePos = Vector2.zero;
@@ -33,7 +52,14 @@ public class CameraSystem : MonoBehaviour
 
         HandleRotation();
 
-        HandleCameraZoom();
+        if (_zoomFOV)
+            HandleCameraZoomFOV();
+
+        if (_zoomFollowOffset)
+            HandleZoomMoveForward();
+
+        if(_zoomLowerY)
+            HandleZoomLowerY();
     }
 
     private void HandleMovement()
@@ -97,9 +123,45 @@ public class CameraSystem : MonoBehaviour
         transform.eulerAngles += new Vector3(0f, rotateDir * _rotateSpeed * Time.deltaTime, 0f);
     }
 
-    private void HandleCameraZoom()
+    private void HandleCameraZoomFOV()
     {
-        _camera.m_Lens.FieldOfView -= Input.mouseScrollDelta.y * _zoomSpeed * Time.deltaTime;
+        _targetFieldOfView -= Input.mouseScrollDelta.y * _zoomSpeed;
+
+        _targetFieldOfView = Mathf.Clamp(_targetFieldOfView, _minFieldOfView, _maxFieldOfView);
+
+        _camera.m_Lens.FieldOfView =
+            Mathf.Lerp(_camera.m_Lens.FieldOfView, _targetFieldOfView, Time.deltaTime * _zoomSmoothing);
+    }
+
+    private void HandleZoomMoveForward()
+    {
+        Vector3 zoomDir = _followOffset.normalized;
+
+        _followOffset -= zoomDir * Input.mouseScrollDelta.y * _zoomSpeed;
+
+        if (_followOffset.sqrMagnitude < _minFollowOffset * _minFollowOffset)
+        {
+            _followOffset = zoomDir * _minFollowOffset;
+        }
+        else if (_followOffset.sqrMagnitude > _maxFollowOffset * _maxFollowOffset)
+        {
+            _followOffset = zoomDir * _maxFollowOffset;
+        }
+
+        var transposer = _camera.GetCinemachineComponent<CinemachineTransposer>();
+        transposer.m_FollowOffset =
+            Vector3.Lerp(transposer.m_FollowOffset, _followOffset, Time.deltaTime * _zoomSmoothing);
+    }
+
+    private void HandleZoomLowerY()
+    {
+        _followOffset.y -= Input.mouseScrollDelta.y * _zoomSpeed;
+
+        _followOffset.y = Mathf.Clamp(_followOffset.y, _minY, _maxY);
+
+        var transposer = _camera.GetCinemachineComponent<CinemachineTransposer>();
+        transposer.m_FollowOffset =
+            Vector3.Lerp(transposer.m_FollowOffset, _followOffset, Time.deltaTime * _zoomSmoothing);
     }
 }
 
