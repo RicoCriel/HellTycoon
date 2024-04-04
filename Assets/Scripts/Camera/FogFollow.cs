4c5c5015fa72f3b3902ebc9d2085d555f0d28c03
CocoTallyman <Lander.Molleman@outlook.com>
2024-04-04 13:54:03 +0200

    Update Mainscene

--- Assets/Scripts/Camera/FogFollow.cs
+++ Assets/Scripts/Camera/FogFollow.cs
@@ -1,21 +1,34 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FogFollow : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject FogPlane;
-    [SerializeField]
-    private GameObject Camera;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject FogPlane;
+    [SerializeField] private GameObject MainCamera;
+    [SerializeField] private List<Material> layerColors;
 
-    // Update is called once per frame
+    private int currentColorIndex = 0;
+    private Color targetColor;
+
     void Update()
     {
-        FogPlane.transform.position = new Vector3(0, Camera.transform.position.y - 50,0);
+        // Update position of FogPlane to follow the camera
+        FogPlane.transform.position = new Vector3(0, MainCamera.transform.position.y - 50, 0);
+
+        // Ensure currentColorIndex is within bounds of layerColors list
+        currentColorIndex = Mathf.Clamp(currentColorIndex, 0, layerColors.Count - 1);
+
+        // Calculate lerped color
+        Color currentColor = FogPlane.GetComponent<Renderer>().material.color;
+        Color lerpedColor = Color.Lerp(currentColor, targetColor, Time.deltaTime*3);
+
+        // Set the fog color property of the material
+        FogPlane.GetComponent<Renderer>().material.color = lerpedColor;
+    }
+
+    public void ChangeColor(int index)
+    {
+        currentColorIndex = index;
+        targetColor = layerColors[currentColorIndex].color;
     }
 }
