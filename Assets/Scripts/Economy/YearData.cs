95c584d846d6b1290ef8d06849a812ce1588c477
FlorisLeirs <floris.leirs@gmail.com>
2024-04-08 16:18:25 +0200

    Added new input system and years

---
+++ Assets/Scripts/Economy/YearData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Economy
+{
+    public class YearData
+    {
+        public int YearNumber = 1;
+
+    }
+}
