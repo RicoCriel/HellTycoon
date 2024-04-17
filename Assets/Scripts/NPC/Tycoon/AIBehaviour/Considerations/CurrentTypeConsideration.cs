c5e1a683b5fccdd585a9c14682088487eb2eb5c6
FlorisLeirs <floris.leirs@gmail.com>
2024-04-17 20:26:36 +0200

    Tycoon chooses stat with highest demand

--- Assets/Scripts/NPC/Tycoon/AIBehaviour/Considerations/CurrentTypeConsideration.cs
+++ Assets/Scripts/NPC/Tycoon/AIBehaviour/Considerations/CurrentTypeConsideration.cs
@@ -2,22 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using Economy;
 using TinnyStudios.AIUtility;
-using Tycoons;
 using UnityEngine;
 
 namespace Tycoons
 {
-    [CreateAssetMenu(menuName = "TinnyStudios/UtilityAI/Considerations/Demand")]
-    public class DemandConsideration : Consideration
+    [CreateAssetMenu(menuName = "TinnyStudios/UtilityAI/Considerations/CurrentProduction")]
+    public class CurrentTypeConsideration : Consideration
     {
         [SerializeField] private StatType _statType;
+
         public override float GetScore(Agent agent, IUtilityAction action)
         {
             var context = agent.GetContext<TycoonDataContext>();
-            if (context != null && context.EconomyManager != null)
+            if (context != null && context.CurrentProduction == _statType)
             {
-                var demand = context.EconomyManager.GetDemand(_statType);
-                return ResponseCurve.Evaluate(demand);
+                return ResponseCurve.Evaluate(1);
             }
 
             return 0.1f;
